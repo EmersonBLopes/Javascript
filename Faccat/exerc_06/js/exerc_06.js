d03480a24919819b284253fdaa63b2aebcf4ac13
EmersonBLopes <101227505+EmersonBLopes@users.noreply.github.com>
2022-04-13 09:12:19 -0300

    Add files via upload

---
+++ Faccat/exerc_06/js/exerc_06.js
@@ -0,0 +1,9 @@
+let base, altura, area;
+
+   base = parseInt(prompt("Digite a base:"));
+   altura = parseInt(prompt("Digite a altura:"));
+
+   area = base * altura;
+
+   alert("A área do rentângulo é: "+area);
+   
