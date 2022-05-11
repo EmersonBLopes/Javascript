742610cdd14e91cd422a33987e896894802fa0bf
EmersonBLopes <101227505+EmersonBLopes@users.noreply.github.com>
2022-05-11 08:24:02 -0300

    Add files via upload

---
+++ Exerc_01/js/Exerc_01_soma.js
@@ -0,0 +1,8 @@
+let numero1, numero2, resultado;
+
+numero1 = parseInt(prompt("Digite um valor:"));
+numero2 = parseInt(prompt("Digite outro valor:"));
+
+resultado = numero1 + numero2;
+
+alert("O resultado da soma é: "+resultado);
